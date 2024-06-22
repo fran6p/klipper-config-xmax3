0b57763d6317d357ea59c9ef881d8a7fd5da1080
mks <mks@mkspi-lGacHc2>
2024-06-22 16:43:59 +0200

    octoeverywhere.conf modified - 06/22/2024 - 12:34:25 PM

---
+++ printer_data/config/scripts/clean_backups.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+#
+# Copyright (C) 2023 Chris Laprade (chris@rootiest.com)
+# 
+# This file is part of config.
+# 
+# config is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# config is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with config.  If not, see <http://www.gnu.org/licenses/>.
+
+KLIPPER_CONFIG_DIR="/home/mks/printer_data/config"
+
+$KLIPPER_CONFIG_DIR/scripts//move_files.sh "$KLIPPER_CONFIG_DIR"/ "$KLIPPER_CONFIG_DIR/backups/" "$KLIPPER_CONFIG_DIR/printer-202*\.cfg"
